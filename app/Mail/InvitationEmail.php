46869ccf37cd244299ace297dc5eb268d10ac902
Helia-Haghighi <helia.haghighi@milele.com>
2024-12-18 11:26:02 +0400

    using Mail instead of PHPMail

--- app/Mail/InvitationEmail.php
+++ app/Mail/InvitationEmail.php
@@ -4,43 +4,24 @@ namespace App\Mail;
 
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
-use Illuminate\Mail\Mailables\Content;
-use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
 class InvitationEmail extends Mailable
 {
     use Queueable, SerializesModels;
 
-    /**
-     * Create a new message instance.
-     */
-    public function __construct(
-        public string $invitationLink,
-        public string $testName
-    ) {}
+    public $invitationLink;
+    public $testName;
 
-    /**
-     * Get the message envelope.
-     */
-    public function envelope(): Envelope
+    public function __construct($invitationLink, $testName)
     {
-        return new Envelope(
-            subject: 'Invitation to Take a Test for Milele Motors'
-        );
+        $this->invitationLink = $invitationLink;
+        $this->testName = $testName;
     }
 
-    /**
-     * Get the message content definition.
-     */
-    public function content(): Content
+    public function build()
     {
-        return new Content(
-            view: 'emails.invitation-email-template',
-            with: [
-                'invitationLink' => $this->invitationLink,
-                'testName' => $this->testName
-            ]
-        );
+        return $this->subject('Invitation to Take a Test for Milele Motors')
+                    ->view('emails.invitation-email-template');
     }
 }
