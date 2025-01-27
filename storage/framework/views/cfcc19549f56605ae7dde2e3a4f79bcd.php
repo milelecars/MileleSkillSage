c8ac45cea2b02f2042e35ce16f6b5f0f78286f3d
Helia-Haghighi <helia.haghighi@milele.com>
2025-01-27 11:52:16 +0400

    Usability improvements

--- storage/framework/views/cfcc19549f56605ae7dde2e3a4f79bcd.php
+++ storage/framework/views/cfcc19549f56605ae7dde2e3a4f79bcd.php
@@ -34,6 +34,16 @@
             autocomplete="current-password" 
             class="shadow appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
         >
+        <!--[if BLOCK]><![endif]--><?php $__errorArgs = ['password'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+            <p class="text-red-500 text-xs mt-1"><?php echo e($message); ?></p>
+        <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?><!--[if ENDBLOCK]><![endif]-->
     </div>
 
 
